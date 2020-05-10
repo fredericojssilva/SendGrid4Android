af4e8546e6bd7f6e8288d2c0c18cc067666eb1c3
Frederico Silva <fredericojssilva@gmail.com>
2020-05-10 21:33:57 +0100

    make it async and provide callback

--- app/src/main/java/net/fredericosilva/sendgrid4android/SendGrid.kt
+++ app/src/main/java/net/fredericosilva/sendgrid4android/SendGrid.kt
@@ -5,18 +5,20 @@ import net.fredericosilva.sendgrid4android.models.Mail
 import net.fredericosilva.sendgrid4android.models.SendGridBody
 
 
-class SendGrid() {
+class SendGrid(val callback: Callback? = null) {
 
-    companion object{
-        fun init(apiKey: String, debug: Boolean)
-        {
+    companion object {
+        fun init(apiKey: String, debug: Boolean) {
             SendGridApiService.init(apiKey, debug)
         }
     }
 
     fun send(mail: Mail) {
-
-        SendGridApiService.sendEmail(mail)
+        SendGridApiService.sendEmail(mail, callback)
     }
 
+    interface Callback {
+        fun onFailure()
+        fun onSuccess()
+    }
 }
