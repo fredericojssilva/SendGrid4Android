6f6c9b6171204aab4e9a6284274091896a18d1b3
Frederico Silva <fredericojssilva@gmail.com>
2020-05-18 18:44:23 +0100

    init sendgrid on sample

--- sample/src/main/java/net/fredericosilva/example/MainActivity.kt
+++ sample/src/main/java/net/fredericosilva/example/MainActivity.kt
@@ -1,18 +1,20 @@
 package net.fredericosilva.example
 
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Base64
 import kotlinx.android.synthetic.main.activity_main.*
 import net.fredericosilva.sendgrid4android.SendGrid
 import net.fredericosilva.sendgrid4android.models.*
 
 class MainActivity : AppCompatActivity() {
 
+    val API_KEY = "#APIKEY"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        SendGrid.init(API_KEY, debug = true)
 
         send.setOnClickListener {
             sendEmail(toEmailEditText.text.toString(), contentEmailEditText.text.toString())
